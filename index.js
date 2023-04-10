55592ea04b029550ac0abb65368e9479536152ae
Joseph Schmitt <codewaggle@gmail.com>
2023-04-09 22:54:07 -0400

    Modify Elements

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const mainElementID = document.getElementById("main");
+
+mainElementID.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Joe is the champion";
+
+document.body.append(newHeader);
